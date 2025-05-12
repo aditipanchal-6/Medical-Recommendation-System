e93c2dab8ec855f92c8673a85e1d116de4198632
Aditi panchal <aditipanchal103@gmail.com>
2025-05-12 23:26:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, url_for, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/members')
+
+def members():
+    return'Welcome to my yt channel hahah'
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person has passed and the marks is"+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed and the marks is"+str(score)
+
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=""
+    if marks<50:
+        result='fail'
+    else:
+        result='success'
+    return redirect(url_for(result, score=marks))
+
+if __name__=='__main__':
+    app.run(debug=True)
