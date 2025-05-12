e311b3f55e5607cd58f2ee7f1b2387eb1a0d0f0c
Aditi panchal <aditipanchal103@gmail.com>
2025-05-13 00:18:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, request
+
 app=Flask(__name__)
 
+
 @app.route('/')
-def welcome():
+def index():
     return render_template('index.html')
 
-@app.route('/members')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def members():
-    return'Welcome to my yt channel hahah'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/success/<int:score>')
-def success(score):
-    return "the person has passed and the marks is"+str(score)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/fail/<int:score>')
-def fail(score):
-    return "the person has failed and the marks is"+str(score)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/results/<int:marks>')
-def results(marks):
-    result=""
-    if marks<50:
-        result='fail'
-    else:
-        result='success'
-    return redirect(url_for(result, score=marks))
-
-if __name__=='__main__':
+if __name__=="__main__":
     app.run(debug=True)
